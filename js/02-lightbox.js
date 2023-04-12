c8487fbe418a82a5865d4865551fadc9dddf59cf
Emil Podkowiński <emil_pod1@interia.pl>
2023-04-12 11:43:43 +0200

    change

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//  console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+galleryItems.forEach((el) => {
+  galleryEl.innerHTML += `<li> <a class="gallery__link" href="${el.oryginal}">
+  <img class="gallery__image" src="${el.preview}" alt="${el.decription}" />
+</a></li>`;
+});
+  
+
+  var lightbox = $('.gallery li a').simpleLightbox({
+    captionDelay: 250,
+    captionsData: "alt", 
+  });
+
